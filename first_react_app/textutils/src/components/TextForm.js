b46a4078d6f592841aaee0d6e3db6487755a2ee9
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-04-21 12:15:34 +0530

    added clear text and remove extra spaces two functions to a website

--- first_react_app/textutils/src/components/TextForm.js
+++ first_react_app/textutils/src/components/TextForm.js
@@ -13,6 +13,15 @@ export default function TextForm(props) {
     let newText = text.toLowerCase();
     setText(newText)
   }
+  const handleClearClick = () => {
+    // console.log("Uppercase was clicked" + text);
+    let newText = '';
+    setText(newText)
+  }
+  const handleExtraSpaces = () => {
+   let newText = text.split(/[ ]+/);
+   setText(newText.join(" "))
+  }
 
   const handleOnChange = (event)=> {
   //  console.log("On change");
@@ -28,6 +37,8 @@ export default function TextForm(props) {
 </div>
 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
 <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+<button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+<button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
     <div className="container my-3">
       <h2>Your text summary</h2>
