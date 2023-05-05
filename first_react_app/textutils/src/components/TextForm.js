a730a47d0e5919dddab80cd1a8345a7eec18c598
YogeshKinekar <yogeshkinekar12@gmail.com>
2023-05-05 14:48:40 +0530

    feat:Added auto dismissing alert messages

--- first_react_app/textutils/src/components/TextForm.js
+++ first_react_app/textutils/src/components/TextForm.js
@@ -7,20 +7,24 @@ export default function TextForm(props) {
     // console.log("Uppercase was clicked" + text);
     let newText = text.toUpperCase();
     setText(newText)
+    props.showAlert("Converted to uppercase!", "success")
   }
   const handleLoClick = () => {
     // console.log("Uppercase was clicked" + text);
     let newText = text.toLowerCase();
     setText(newText)
+    props.showAlert("Converted to lowercase!", "success")
   }
   const handleClearClick = () => {
     // console.log("Uppercase was clicked" + text);
     let newText = '';
     setText(newText)
+    props.showAlert("Text Cleared!", "success")
   }
   const handleExtraSpaces = () => {
    let newText = text.split(/[ ]+/);
    setText(newText.join(" "))
+   props.showAlert("Extra spaces removed!", "success")
   }
 
   const handleOnChange = (event)=> {
