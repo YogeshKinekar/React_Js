d63aa76d17a472c13e982bb7cfe04ad2c0e8976e
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-06-19 23:40:40 +0530

    feat: Added state in class based component

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageurl, newsurl} = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src="https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" className="card-img-top" alt="" />
+          <img src={imageurl} className="card-img-top" alt="" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail" className="btn btn-primary">Read More</a>
           </div>
         </div>
       </div>
