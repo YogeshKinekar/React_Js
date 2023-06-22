2108f061239eddaccb5d38b6eaf285c82aa6e004
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-06-23 03:08:03 +0530

    feat: Added API to populate NewsItems

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -6,10 +6,10 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageurl} className="card-img-top" alt="" />
+          <img src={!imageurl?"https://scitechdaily.com/images/Euclid-Universe-scaled.jpg": imageurl} className="card-img-top" alt="" />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a href={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
           </div>
         </div>
